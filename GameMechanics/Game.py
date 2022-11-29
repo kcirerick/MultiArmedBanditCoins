bd84d0959da7e339e88aed9a366f7915be28c2a4
kcirerick <enriquezerick182@gmail.com>
2022-11-28 20:00:31 -0800

    Basic numCoins check and delete coinCollection class.

--- GameMechanics/Game.py
+++ GameMechanics/Game.py
@@ -1,5 +1,7 @@
 class Game:
     def __init__(self, numCoins=10, numTries=100):
+        if numCoins <= 0 or numCoins >= 50: # numCoins in [0,50)
+            numCoins = 10
         self.numCoins = numCoins
         self.numTries = numTries
         self.score = 0
