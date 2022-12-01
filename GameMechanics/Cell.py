64d260306f732639c057246803a116de4c9f22d7
kcirerick <enriquezerick182@gmail.com>
2022-11-30 20:08:00 -0800

    Add self to methods and correct imports.

--- GameMechanics/Cell.py
+++ GameMechanics/Cell.py
@@ -1,5 +1,6 @@
 # Logical abstraction for each interactable unit in the game displaying statistics for each coin over time.abstraction
 import random
+from .Coin import Coin
 class Cell:
     def __init__(self):
         self.coin = Coin(bias = random.random())
@@ -8,19 +9,19 @@ class Cell:
         self.totalHeads = 1
         self.trend = [0.5]
 
-    def getTotalHeads():
+    def getTotalHeads(self):
         return self.totalHeads - 1 # Subtract one in the display to hide laplacian prior
 
-    def getTotalFlips():
+    def getTotalFlips(self):
         return self.totalFlips - 2 # Subtract two in the display to hide the laplacian prior
 
-    def getCoinEstimate():
+    def getCoinEstimate(self):
         return self.totalHeads / totalFlips
 
-    def getTrend():
+    def getTrend(self):
         return self.trend
 
-    def flipCoin():
+    def flipCoin(self):
         self.totalFlips += 1
         self.totalHeads += self.coin.flip() # flip will either be zero or one
         self.trend.append(self.getCoinEstimate)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from GameMechanics import *
+from GameMechanics.Cell import Cell
+from GameMechanics.Coin import Coin
+from GameMechanics.Game import Game
 import streamlit as st
 """
 Welcome to coin explorer. In this game, your goal is to try to flip as many heads as possible by doing 100 flips of 10 biased coins
@@ -8,14 +10,14 @@ or a 90% bias towards heads. The best you can do is flip the coins and find out!
 to flip. This game involves a little bit of luck, but making good choices can certainly increase your surface area for serendipity!
 """
 NUM_COINS = 1
-cells = [Cell() for i in range(NUM_COINS)]
-for cell in cells:
-    button = st.button("Coin")
-    st.write(button, on_click = activate_cell(cell))
-
 def activate_cell(cell):
     cell.flipCoin()
     self.getTotalHeads() # Empty values can be assumed to be generic st.write() calls.
     self.getTotalFlips()
     self.getCoinEstimate()
     st.line_chart(self.trend())
+
+cells = [Cell() for i in range(NUM_COINS)]
+for cell in cells:
+    button = st.button("Coin")
+    st.write(button, on_click = activate_cell(cell))
