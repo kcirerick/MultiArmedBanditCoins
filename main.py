748150337e38c2413fd26d7b40c5a4fdcb7062cc
kcirerick <enriquezerick182@gmail.com>
2023-01-09 21:40:34 -0800

    Add basic functionality for flipping and updating coins. Next time, fix update_prior() function and update line charts

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ import numpy as np
 # Set the number of coins and number of flips
 NUM_COINS = 10
 NUM_FLIPS = 100
-NUM_ROWS = 5
+NUM_ROWS = 1
 NUM_COLS = 2
 
 # Initialize an array to track the number of heads for each coin
-heads = np.zeros(NUM_COINS)
+heads, flips = np.zeros(NUM_COINS), np.zeros(NUM_COINS)
 
 # Initialize an array to track the biases of the coins
 biases = np.random.rand(NUM_COINS)
 
-# Initialize an array to track the bayesian priors for each coin's bias
-priors = np.ones(NUM_COINS) / 2
+# Initialize an array to track the bayesian priors for each coin's bias,
+priors = [[0.5] for i in range(NUM_COLS*NUM_ROWS)]
 
-def update_game():
-    pass
+def update_game(button_num, coin_bias):
+    result = flip_coin(coin_bias)
+    flips[button_num]+=1
+    heads[button_num]+=result
+    priors[button_num].append(update_prior(priors[button_num][-1], heads[button_num], int(flips[button_num])))
+    st.write(priors)
 
 def update_line_chart():
     print("Confirmed")
@@ -32,34 +36,38 @@ def update_prior(prior, heads, flips):
     # Compute the probability of getting the observed number of heads
     p_observed = np.math.comb(flips, int(heads))
     # Update the bayesian prior using Bayes' theorem
+    st.write((p_observed * p_heads) / (p_observed * p_heads + p_observed * p_tails))
     return (p_observed * p_heads) / (p_observed * p_heads + p_observed * p_tails)
 
 # Create a function to flip a coin with a given bias and update the
 # number of heads for that coin
-def flip_coin(bias, heads):
-    pass
+def flip_coin(bias):
     # Flip a coin with the given bias and update the number of heads
     if np.random.rand() < bias:
-        heads += 1
-    return heads
+        return 1
+    return 0
 
 def create_containers():
     with st.container():
         col1, col2 = st.columns(2)
         with col1:
+            button_num=0
             st.button(
-                        label = "H".format(0, 0),
-                        key = "Button {}-{}".format(0, 0)
-#                         on_click = update_game(),
-#                         args = (i, j, line_charts[i][j])
-                    )
+              label = "H",
+              key = "Button-{}".format(button_num),
+              on_click = update_game,
+              args = (button_num, biases[button_num],)
+            )
+            st.line_chart(priors[0])
         with col2:
+            button_num=1
             st.button(
-                                    label = "H".format(0, 1),
-                                    key = "Button {}-{}".format(0, 1)
-            #                         on_click = update_game(),
-            #                         args = (i, j, line_charts[i][j])
-                                )
+               label = "H",
+               key = "Button-{}".format(button_num),
+               on_click = update_game,
+               args = (button_num, biases[button_num],)
+            )
+            st.line_chart(priors[1])
     return #containers, grid
 
 # Create a function to draw the current state of the game
@@ -93,6 +101,7 @@ while True:
     # Draw the current state of the game
     draw_game(biases, heads, priors)
     # Check if the game is over
+    break
     if np.sum(heads) == NUM_FLIPS:
         # Compute the total number of heads
         total_heads = np.sum(heads)
